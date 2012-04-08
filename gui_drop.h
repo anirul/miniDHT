ac24e24d876c93ece864e108881665cff0ca0f3a
Micmac2 <micmac2@mac.com>
2012-04-08 18:14:22 +0200

    Drag'n drop support for Mac OS X and other plateform ? Preliminary support for 'magnet type' link for OS X.

---
+++ gui_drop.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2011, mic
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the CERN nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY anirul ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL anirul BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef MINIDHT_GUI_DROP_HEADER_DEFINED
+#define MINIDHT_GUI_DROP_HEADER_DEFINED
+
+#include <wx/wx.h>
+#include <wx/dnd.h>
+#include <boost/function.hpp>
+
+class gui_drop : public wxFileDropTarget {
+    boost::function<bool(const wxArrayString&)> onDrop_;
+public:
+    gui_drop(boost::function<bool(const wxArrayString& filenames)> onDrop);
+    bool OnDropFiles(wxCoord x, wxCoord y, const wxArrayString& filenames);
+
+};
+
+#endif
